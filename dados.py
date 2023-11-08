b586808d995649f27cd8a8a83ab296bbf91ee29e
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2023-11-07 21:59:11 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,55 @@
+# Dados simulados de cargos nas áreas
+cargos_por_area = {
+      "":[],
+      "web": ["Frontend Developer", "Web Designer"],
+      "dados": ["Cientista de Dados Júnior", "Cientista de Dados Sênior"],
+      "ia": ["Engenheiro de IA"],
+      "mobile": ["Desenvolvedor Mobile", "Engenheiro de Software Mobile"]
+  }
+requisitos_vagas = {
+    "frontend": {
+        "Linguagens": "HTML, CSS, JavaScript",
+        "Framework": "React, Angular, Vue.js",
+        "HardSkill": "design de interface do usuário, teste unitários, Git, Escalabilidade, NPM",
+        "Soft Skills": "Trabalho em equipe, Comunicação objetiva, Adaptabilidade",
+    },
+    "backend": {
+        "Linguagens": "Node.js, Python, Ruby",
+        "Banco de Dados": "SQL, NoSQL",
+        "Framework": "Express.js, Django, Ruby on Rails",
+        "HardSkill": "APIs RESTful, segurança, escalabilidade, modelagem de banco de dados",
+        "Soft Skills": "Resolução de problemas, Trabalho em equipe, Comunicação eficaz",
+    },
+    "fullstack": {
+        "Linguagens": "HTML, CSS, JavaScript, Node.js, Python, Ruby",
+        "Banco de Dados": "SQL, NoSQL",
+        "Framework": "React, Angular, Vue.js, Express.js, Django, Ruby on Rails",
+        "HardSkill": "design de interface do usuário, teste unitários, Git, Escalabilidade, modelagem de banco de dados",
+        "Soft Skills": "Trabalho em equipe, Comunicação objetiva, Resolução de problemas",
+    },
+    "cientista de dados": {
+        "Linguagens": "Python, R",
+        "Machine Learning": "Scikit-Learn, TensorFlow, PyTorch",
+        "Estatísticas": "Análise estatística, inferência estatística",
+        "Ferramentas": "Jupyter Notebook, Pandas, Numpy",
+        "Soft Skills": "Pensamento analítico, Comunicação eficaz, Resolução de problemas",
+    },
+    "power bi": {
+        "Conhecimentos": "Criação de relatórios, visualizações e dashboards no Power BI, DAX, modelagem de dados no Power BI",
+        "Soft Skills": "Pensamento analítico, Comunicação de dados, Resolução de problemas",
+    },
+    "qa": {
+        "Testes": "Testes de software, automação de testes, depuração",
+        "Ferramentas": "Ferramentas de teste, testes de regressão, relatórios de defeitos",
+        "Soft Skills": "Resolução de problemas, Comunicação eficaz, Pensamento analítico",
+    },
+    "analista de sistema": {
+        "Análise de Requisitos": "Análise de requisitos, design de sistemas, documentação",
+        "Soft Skills": "Trabalho em equipe, Comunicação eficaz, Resolução de problemas",
+    },
+    "ui/ux": {
+        "Design de Interface": "UI/UX Design, Design de Interação",
+        "Ferramentas": "Adobe XD, Figma, Sketch",
+        "Soft Skills": "Pensamento analítico, Criatividade, Comunicação de design",
+    }
+}
